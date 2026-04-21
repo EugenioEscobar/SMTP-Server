e58c21f7f39a2ee1fa9ffc67e5c9e9c564baa90d
Euge Escobar <eugenio.escobar18@gmail.com>
2026-04-20 22:53:15 -0400

    Se configura el server para CPanel (prueba)

--- server.js
+++ server.js
@@ -194,14 +194,4 @@ fastify.post('/contacto', async (request, reply) => {
 })
 
 // Levantar servidor
-const start = async () => {
-  try {
-    await fastify.listen({ port: 3000, host: '0.0.0.0' })
-    console.log('Servidor corriendo en http://localhost:3000')
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-
-start()
+export default fastify
